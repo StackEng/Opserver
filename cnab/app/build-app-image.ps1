9282143b4c914770a19dd4320e60b9b801ac41fe
Wouter de Kort <wouter@wouterdekort.com>
2024-11-14 11:10:21 +0100

    Setup different secrets for Exceptional and Status

--- cnab/app/build-app-image.ps1
+++ cnab/app/build-app-image.ps1
@@ -1,5 +1,5 @@
 function Build-Local-App-Image() {
 
       
-    docker build -t local.software/stackeng/opserver/opserver -t cr.stackoverflow.software/stackeng/opserver/opserver:local .
+    # docker build -t local.software/stackeng/opserver/opserver -t cr.stackoverflow.software/stackeng/opserver/opserver:local .
 }
